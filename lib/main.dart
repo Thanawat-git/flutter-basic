8de807250634a5b5abdfa18c0da0414b58b56997
Thanawat.git <buysai123gw@gmail.com>
2023-06-11 13:05:49 +0700

    dynamic width display

--- lib/main.dart
+++ lib/main.dart
@@ -72,30 +72,32 @@ class _MyHomePageState extends State<MyHomePage> {
       default:
         throw UnimplementedError("no widget for $selectedIndex");
     }
-    return Scaffold(
-        body: Row(
-      children: [
-        SafeArea(
-            child: NavigationRail(
-          extended: false,
-          destinations: [
-            NavigationRailDestination(
-                icon: Icon(Icons.home), label: Text("Home")),
-            NavigationRailDestination(
-              icon: Icon(Icons.favorite),
-              label: Text("Favorites"),
-            ),
-          ],
-          selectedIndex: selectedIndex,
-          onDestinationSelected: onDestinationSelected,
-        )),
-        Expanded(
-            child: Container(
-          color: Theme.of(context).colorScheme.primaryContainer,
-          child: page,
-        ))
-      ],
-    ));
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+          body: Row(
+        children: [
+          SafeArea(
+              child: NavigationRail(
+            extended: constraints.maxWidth >= 600, // like media query -> check screen size.
+            destinations: [
+              NavigationRailDestination(
+                  icon: Icon(Icons.home), label: Text("Home")),
+              NavigationRailDestination(
+                icon: Icon(Icons.favorite),
+                label: Text("Favorites"),
+              ),
+            ],
+            selectedIndex: selectedIndex,
+            onDestinationSelected: onDestinationSelected,
+          )),
+          Expanded(
+              child: Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: page,
+          ))
+        ],
+      ));
+    });
   }
 }
 
